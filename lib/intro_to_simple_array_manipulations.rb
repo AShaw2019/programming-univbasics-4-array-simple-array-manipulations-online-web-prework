b11210aaa4c170bb95613ad693efa05d33ef25a7
Ashley Twomey <aprier091012@gmail.com>
2019-09-04 14:48:01 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,3 +2,6 @@ def famous_cats = ["lil' bub", "grumpy cat", "Maru"]
  famous_cats.push("nala cat")
 end
 
+def famous_cats = ["lil' bub", "grumpy cat", "Maru"]
+ famous_cats.unshift("nala cat")
+end
